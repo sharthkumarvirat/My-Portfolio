f360680495c72f69eb0f4cd5a6d14fa23701e5e8
Sharath Kumar M N <150372586+sharthkumarvirat@users.noreply.github.com>
2024-02-16 00:19:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const navbar = document.querySelector('.header .navbar');
+const menuButton = document.querySelector('.header .menu');
+
+menuButton.addEventListener('click', () => {
+  navbar.classList.toggle('show');
+});
+
+document.onscroll = () => {
+  navbar.classList.remove('show');
+
+  if (window.scrollY > 0) {
+    document.querySelector('.header').classList.add('active');
+  } else {
+    document.querySelector('.header').classList.remove('active');
+  }
+};
+
+document.onload = () => {
+  if (window.scrollY > 0) {
+    document.querySelector('.header').classList.add('active');
+  } else {
+    document.querySelector('.header').classList.remove('active');
+  }
+};
+
+function sendMail(){
+  var subject = document.getElementById("subject").value;
+  var mailBody = document.getElementById("mailBody").value;
+  window.location.href = "mailto:sharathkumarmn2001@gmail.com?subject=" + subject + "&body=" + mailBody;
+}
